f586641bdb9730f709ff5358a41922af5fe3b751
Riya-JAIN2001 <167556137+Riya-JAIN2001@users.noreply.github.com>
2024-05-16 23:43:03 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -35,6 +35,7 @@ const Connect=async ()=>{
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
+    next();
 }) 
 app.use(express.json());
 app.use(cors(corsOptions));
